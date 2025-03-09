f50a4cccf6a5e4d5e7fe6026c2cf9b2fc590833c
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-09 09:41:56 +0530

    added navbar.jsx

--- BusTicket_website/src/App.jsx
+++ BusTicket_website/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Router>
         <main className="w-full flex flex-col bg-neutral-50 min-h-screen">
-          This is main section Footer
+          <Navbar />
         </main>
       </Router>
     </>
