b6d7a700978036f9948c5f08d7bda5081bfa8b87
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-09 11:49:22 +0530

    update

--- BusTicket_website/src/Components/Navbar/Navbar.jsx
+++ BusTicket_website/src/Components/Navbar/Navbar.jsx
@@ -1,39 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa6";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const navItems = [
+    { label: "Home", link: "/" },
+    { label: "Services", link: "/services" },
+    { label: "Tickets", link: "/tickets" },
+    { label: "About", link: "/about" },
+  ];
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <nav className="w-full h-[8ch] fixed top-0 left-0 lg:px-24 md:px-16 sm:px-16 sm:px-7 px-4 backdrop-blur-1g transition-transform duration-300 z-50">
+    <nav className="w-full h-[8ch] fixed top-0 left-0 lg:px-24 md:px-16 sm:px-7 px-4 backdrop-blur-lg transition-transform duration-300 z-50">
       <div className="w-full h-full flex items-center justify-between">
         <Link to="/" className="text-4xl text-red-500 font-bold">
           Bus
         </Link>
 
-        <div className="w-fit md:hidden flex items-center justify-center cursor-pointer flex-col gap-1 text-neutral-700">
-          //FaBars
+        <div
+          className="w-fit md:hidden flex items-center justify-center cursor-pointer flex-col gap-1 text-neutral-700"
+          onClick={handleOpen}
+        >
+          {open ? <FaBars /> : <FaBars />}
         </div>
 
-        <div className="flex-1 w-full flex items-center justify-end gap-16">
+        <div
+          className={` ${
+            open
+              ? "flex absolute top-20 left-0 w-full h-auto md:relative"
+              : "hidden"
+          } flex-1 md:flex flex-col md:flex-row md:flex-row md:gap-14 gap-8 md:items-center items-start md:bg-transparent bg-neutral-50 md:shadow-none sm:shadow-md shadow-md rounded-xl `}
+        >
           <ul className="list-none flex md:items-center items-start flex-wrap md:flex-row flex-col md:gap-8 gap-4 text-lg text-neutral-500 font-normal">
-            <li>
-              <Link
-                to="/"
-                className="hover:text-red-500 ease-in-out duration-300"
-              >
-                Home
-              </Link>
-            </li>
+            {navItems.map((item, ind) => (
+              <li key={ind}>
+                <Link
+                  to={item.link}
+                  className="hover:text-red-500 ease-in-out duration-300"
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
           </ul>
 
           <div className="flex items-center justify-center">
-            <button className="md:w-fit w-full md:px-4 px-6 md:py-1 py-2 5 bg-red-500 border border-red-500 hover:border-red-500 md:rounded-full rounded-xl text-base font-normal text-neutral-50 hover:text-red-500 ease-in-out duration:300">
-              Sig In
+            <button className="md:w-fit w-full md:px-4 px-6 md:py-1 py-2.5 bg-red-500 border border-red-500 hover:border-red-500 md:rounded-full rounded-xl text-base font-normal text-neutral-50 hover:text-red-500 ease-in-out duration-300">
+              Sign In
             </button>
           </div>
-          <div className="flex items-center justify-center"></div>
         </div>
       </div>
     </nav>
   );
 };
+
 export default Navbar;
