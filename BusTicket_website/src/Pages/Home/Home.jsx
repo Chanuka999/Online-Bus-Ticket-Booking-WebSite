29867f61bcb1937a3b73b73d2bd0962dc4bb2ec6
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-09 14:32:21 +0530

    update

---
+++ BusTicket_website/src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="h-screen w-full flex items justify-center">
+      <h1 className="text-5xl text-neural-950 font-bold"></h1>
+    </div>
+  );
+};
+
+export default Home;
