c6940125aac18aee83a8efc3f55dd159b0c47235
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-10 09:50:22 +0530

    added about page

--- BusTicket_website/src/App.jsx
+++ BusTicket_website/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
+import About from "./Pages/Home/About/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
           </Routes>
         </main>
       </Router>
