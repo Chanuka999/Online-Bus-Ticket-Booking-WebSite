90b329d8200dbaf4eae5d628a8646808ac6890cd
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-10 09:50:38 +0530

    update

--- BusTicket_website/src/Pages/Home/Home.jsx
+++ BusTicket_website/src/Pages/Home/Home.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="h-screen w-full flex items justify-center">
-      <h1 className="text-5xl text-neural-950 font-bold"></h1>
+    <div className="min-h-screen w-full flex items-center justify-center flex-col">
+      <div className="w-full h-screen flex items-center justify-center">
+        <h1 className="text-5xl text-neutral-950 font-bold">
+          This is home page
+        </h1>
+      </div>
+
+      <div className="w-full h-screen flex items-center justify-center bg-neutral-950">
+        <h1 className="text-5xl text-neutral-50 font-bold">
+          This is about page
+        </h1>
+      </div>
     </div>
   );
 };
