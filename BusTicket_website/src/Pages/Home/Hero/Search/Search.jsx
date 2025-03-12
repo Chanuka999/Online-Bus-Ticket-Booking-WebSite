e12e23cedcd70aa18e39824d7e6eb9db6d1dd263
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-12 11:23:42 +0530

    add to service page

--- BusTicket_website/src/Pages/Home/Hero/Search/Search.jsx
+++ BusTicket_website/src/Pages/Home/Hero/Search/Search.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { TbArrowExchange } from "react-icons/tb";
+import { TbArrowsExchange } from "react-icons/tb";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 
 const Search = () => {
   return (
@@ -13,16 +15,45 @@ const Search = () => {
     >
       <div className="w-full flex items-center gap-5 justify-between">
         <div className="w-[60%] flex items-center gap-5 relative">
-          <div className="w-1/2 h-14 border border-neutral-300 bg-white/70 text-base text-neutral-700 font-medium px-5 flex items-center gap-x1 rounded-lg"></div>
+          <div className="w-1/2 h-14 border border-neutral-300 bg-white/70 text-base text-neutral-700 font-medium px-5 flex items-center gap-x-1 rounded-lg">
+            <input
+              type="text"
+              placeholder="From..."
+              className="flex-1 h-full border-none bg-transparent foucs:outline-none"
+            />
+            <div className="w-5 h-5 text-neutral-400">
+              <FaMapMarkerAlt className="w-full h-full" />
+            </div>
+          </div>
+          <div className="w-1/2 h-14 border border-neutral-300 bg-white/70 text-base text-neutral-700 font-medium px-5 flex items-center gap-x-1 rounded-lg">
+            <input
+              type="text"
+              placeholder="To..."
+              className="flex-1 h-full border-none bg-transparent foucs:outline-none"
+            />
+            <div className="w-5 h-5 text-neutral-400">
+              <FaMapMarkerAlt className="w-full h-full" />
+            </div>
+          </div>
 
-          <div className="w-1/2 h-14 border border-neutral-300 bg-white/70 text-base text-neutral-700 font-medium px-5 flex items-center gap-x1 rounded-lg"></div>
-
-          <button className="absolute w-11 h-6 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full flex items-center justify-center bg-primary">
-            <TbArrowExchange className="w-6 h-6 " />
+          <button className="absolute w-11 h-6 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full flex items-center justify-center bg-red-500">
+            <TbArrowsExchange className="w-6 h-6 text-neutral-50" />
           </button>
         </div>
 
-        <div className="flex-1 h-14 flex items-center gap-5"></div>
+        <div className="flex-1 h-14 flex items-center gap-5">
+          <div className="flex-1 h-full border border-neutral-300 bg-white/70 text-base text-neutral-700 font-medium px-5 flex items-center gap-x-1 rounded-lg">
+            <input
+              type="date"
+              className="flex-1 h-full border-none bg-transparent foucs:outline-none"
+            />
+          </div>
+
+          <button className="w-fit px-5 h-full bg-red-500 hover:bg-transparent border-2 border-primary hover:border-primary rounded-xl text-base font-medium text-neutral-50 flex items-center justify-center gap-x-2 hover:text-primary ease-in-out duration-300">
+            <FaSearch />
+            Search
+          </button>
+        </div>
       </div>
     </motion.div>
   );
