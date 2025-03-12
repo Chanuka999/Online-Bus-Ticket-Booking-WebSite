939aa1c1781b711979bab94573f37946db92286d
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-12 11:23:26 +0530

    update

--- BusTicket_website/src/Components/Navbar/Navbar.jsx
+++ BusTicket_website/src/Components/Navbar/Navbar.jsx
@@ -39,7 +39,8 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`w-full h-[8ch] fixed top-0 left-0 lg:px-24 md:px-16 sm:px-7 px-4 backdrop-blur-lg transition-transform duration-300 z-50 ${
+      //backdrop-blur-lg
+      className={`w-full h-[8ch] fixed top-0 left-0 lg:px-24 md:px-16 sm:px-7 px-4  transition-transform duration-300 z-50 ${
         isVisible ? "translate-y-0" : "translate-y-full"
       } ${scrollPosition > 50 ? "bg-violet-200" : "bg-neutral-100/10"}`}
     >
