2d0aa2639d991457af77d6942162642bf7f2fec2
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-12 15:50:08 +0530

    update

--- BusTicket_website/src/Pages/Home/Home.jsx
+++ BusTicket_website/src/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "./Hero/Hero";
 import Services from "./Services/Services";
+import TopSearch from "./TopSearch/TopSearch";
 
 const Home = () => {
   return (
-    <div className="space-y-16 w-full min-h-screen pb-16">
+    <div className="space-y-24 w-full min-h-screen pb-24 px-8 ">
       <Hero />
+
       <Services />
+
+      <TopSearch />
     </div>
   );
 };
