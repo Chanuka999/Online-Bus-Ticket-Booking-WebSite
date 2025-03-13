ec35328cacf22f5e50cc29f6496a1b70b3819770
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-13 19:07:22 +0530

    create footer.jsx

---
+++ BusTicket_website/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RootLayout from "../Layout/RootLayout";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-auto bg-neutral-950 py-12">
+      <RootLayout className="space-y-10"></RootLayout>
+    </div>
+  );
+};
+
+export default Footer;
