d7b0e67f387111cfe55879a355c2699c2a690d22
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-24 22:25:10 +0530

    create new page

---
+++ BusTicket_website/src/Components/Layout/Toppage/TopLayout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const TopLayout = ({ bgImg, title, className }) => {
+  return <motion.div></motion.div>;
+};
+
+export default TopLayout;
