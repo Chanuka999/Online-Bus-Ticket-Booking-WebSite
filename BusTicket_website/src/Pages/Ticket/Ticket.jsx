c6a53e7a732b343dcd09915d59fc0d597f8adaba
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-28 15:43:00 +0530

    new page created

--- BusTicket_website/src/Pages/Ticket/Ticket.jsx
+++ BusTicket_website/src/Pages/Ticket/Ticket.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import TopLayout from "../../Components/Layout/Toppage/TopLayout";
+import RootLayout from "../../Components/Layout/RootLayout";
+import { motion } from "framer-motion";
+import Search from "../Home/Hero/Search/Search";
+import Filter from "./Filter/Filter";
 
 const Ticket = () => {
   return (
@@ -10,6 +14,28 @@ const Ticket = () => {
         }
         title={"Reserve Your ticket"}
       />
+
+      <RootLayout>
+        <div className="space-y-5 w-full bg-neutral-50 flex py-4 items-center justify-center flex-col sticky top-0 z-30">
+          <motion.h1
+            initial={{ opacity: 0, y: -800 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -800 }}
+            transition={{ duration: 1.85, ease: "easeOut" }}
+            className="text-3xl text-neutral-700 font-semibold"
+          >
+            want to change the route?
+          </motion.h1>
+
+          <Search />
+        </div>
+
+        <div className="w-full h-auto grid grid-cols-4 gap-16 relative">
+          <div className="col-span-1">
+            <Filter className="space-y-4 sticky top-52" />
+          </div>
+        </div>
+      </RootLayout>
     </div>
   );
 };
