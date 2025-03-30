fd3d2061319e424c4b73559ea44a376e09b249c2
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-30 10:47:21 +0530

    update code

--- BusTicket_website/src/Pages/Ticket/Filter/Filter.jsx
+++ BusTicket_website/src/Pages/Ticket/Filter/Filter.jsx
@@ -18,36 +18,151 @@ const Filter = ({ className }) => {
         <h1 className="text-lg text-neutral-600 font-medium">Apply Filters</h1>
 
         <PriceRangeSlider min={1000} max={3000} onChange={handleRangeChange} />
-      </div>
 
+        <div className="w-full border border-neutral-300 rounded-xl p-4 space-y-1">
+          <h1 className="text-lg text-neutral-600 font-medium">Bus Types</h1>
+          <div className="space-y-2.5">
+            <div className="w-full flex items-center gap-2">
+              <input
+                type="checkbox"
+                id=""
+                className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
+              />
+              <label
+                htmlFor="ac"
+                className="text-sm text-neutral-600 font-normal cursor-pointer"
+              >
+                AC Deluxe<span className="text-xs text-neutral-600">(10)</span>
+              </label>
+            </div>
+
+            <div className="w-full flex items-center gap-2">
+              <input
+                type="checkbox"
+                id="tourist"
+                className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
+              />
+              <label
+                htmlFor="tourist"
+                className="text-sm text-neutral-600 font-normal cursor-pointer"
+              >
+                Tourist AC Deluxe
+                <span className="text-xs text-neutral-600">(10)</span>
+              </label>
+            </div>
+
+            <div className="w-full flex items-center gap-2">
+              <input
+                type="checkbox"
+                id="airSuspension"
+                className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
+              />
+              <label
+                htmlFor="airSuspension"
+                className="text-sm text-neutral-600 font-normal cursor-pointer"
+              >
+                Air suspension
+                <span className="text-xs text-neutral-600">(10)</span>
+              </label>
+            </div>
+
+            <div className="w-full flex items-center gap-2">
+              <input
+                type="checkbox"
+                id="luxuryac"
+                className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
+              />
+              <label
+                htmlFor="luxuryac"
+                className="text-sm text-neutral-600 font-normal cursor-pointer"
+              >
+                luxury AC Deluxe
+                <span className="text-xs text-neutral-600">(10)</span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="w-full border border-neutral-300 rounded-xl p-4 space-y-1">
+          <h1 className="text-lg text-neutral-600 font-medium">
+            Bus Companies
+          </h1>
+          <div className="space-y-2.5">
+            <div className="w-full flex items-center gap-2">
+              <input
+                type="checkbox"
+                id=""
+                className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
+              />
+              <label
+                htmlFor="sworgadwarideluxe"
+                className="text-sm text-neutral-600 font-normal cursor-pointer"
+              >
+                sworgadwari deluxe
+                <span className="text-xs text-neutral-600">(10)</span>
+              </label>
+            </div>
+
+            <div className="w-full flex items-center gap-2">
+              <input
+                type="checkbox"
+                id="tourist"
+                className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
+              />
+              <label
+                htmlFor="pokkharadeluxe"
+                className="text-sm text-neutral-600 font-normal cursor-pointer"
+              >
+                pokhara delux
+                <span className="text-xs text-neutral-600">(10)</span>
+              </label>
+            </div>
+
+            <div className="w-full flex items-center gap-2">
+              <input
+                type="checkbox"
+                id="airSuspension"
+                className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
+              />
+              <label
+                htmlFor="mustangdelux"
+                className="text-sm text-neutral-600 font-normal cursor-pointer"
+              >
+                Mustang Delux
+                <span className="text-xs text-neutral-600">(10)</span>
+              </label>
+            </div>
+
+            <div className="w-full flex items-center gap-2">
+              <input
+                type="checkbox"
+                id="luxuryac"
+                className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
+              />
+              <label
+                htmlFor="lumbinidelux"
+                className="text-sm text-neutral-600 font-normal cursor-pointer"
+              >
+                lumbini Delux
+                <span className="text-xs text-neutral-600">(10)</span>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="w-full border border-neutral-300 rounded-xl p-4 space-y-1">
-        <h1 className="text-lg text-neutral-600 font-medium">Bus Types</h1>
+        <h1 className="text-lg text-neutral-600 font-medium">Amenities</h1>
         <div className="space-y-2.5">
           <div className="w-full flex items-center gap-2">
             <input
               type="checkbox"
-              id=""
+              id="internet"
               className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
             />
             <label
-              htmlFor="ac"
+              htmlFor="internet"
               className="text-sm text-neutral-600 font-normal cursor-pointer"
             >
-              AC Deluxe<span className="text-xs text-neutral-600">(10)</span>
-            </label>
-          </div>
-
-          <div className="w-full flex items-center gap-2">
-            <input
-              type="checkbox"
-              id="tourist"
-              className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
-            />
-            <label
-              htmlFor="tourist"
-              className="text-sm text-neutral-600 font-normal cursor-pointer"
-            >
-              Tourist AC Deluxe
+              Internet/wifi
               <span className="text-xs text-neutral-600">(10)</span>
             </label>
           </div>
@@ -55,14 +170,14 @@ const Filter = ({ className }) => {
           <div className="w-full flex items-center gap-2">
             <input
               type="checkbox"
-              id="airSuspension"
+              id="acairsusp"
               className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
             />
             <label
-              htmlFor="airSuspension"
+              htmlFor="acairsusp"
               className="text-sm text-neutral-600 font-normal cursor-pointer"
             >
-              Air suspension
+              AC and Air suspention
               <span className="text-xs text-neutral-600">(10)</span>
             </label>
           </div>
@@ -70,21 +185,49 @@ const Filter = ({ className }) => {
           <div className="w-full flex items-center gap-2">
             <input
               type="checkbox"
+              id="tvmusic"
+              className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
+            />
+            <label
+              htmlFor="tvmusic"
+              className="text-sm text-neutral-600 font-normal cursor-pointer"
+            >
+              LED TV & Music
+              <span className="text-xs text-neutral-600">(10)</span>
+            </label>
+          </div>
+
+          <div className="w-full flex items-center gap-2">
+            <input
+              type="chargingport"
               id="luxuryac"
               className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
             />
             <label
-              htmlFor="luxuryac"
+              htmlFor="chargingport"
               className="text-sm text-neutral-600 font-normal cursor-pointer"
             >
-              luxury AC Deluxe
+              Charging Port
+              <span className="text-xs text-neutral-600">(10)</span>
+            </label>
+          </div>
+
+          <div className="w-full flex items-center gap-2">
+            <input
+              type="fan"
+              id="luxuryac"
+              className="h-3.5 w-3.5 border border-neutral-300 text-neutral-300 cursor-pointer"
+            />
+            <label
+              htmlFor="fan"
+              className="text-sm text-neutral-600 font-normal cursor-pointer"
+            >
+              Fan
               <span className="text-xs text-neutral-600">(10)</span>
             </label>
           </div>
         </div>
       </div>
-
-      <div className="w-full border border-neutral-300 rounded-xl p-4 space-y-1"></div>
     </div>
   );
 };
