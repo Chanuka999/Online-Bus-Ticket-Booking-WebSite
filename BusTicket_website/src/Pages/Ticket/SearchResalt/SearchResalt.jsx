da7cc95c4b36cf38039dc1b80c853a9d4a6bcb6e
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-30 10:47:31 +0530

    update code

---
+++ BusTicket_website/src/Pages/Ticket/SearchResalt/SearchResalt.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchResalt = () => {
+  return (
+    <div className="w-full col-span-3 space-y-10 pt-11">
+      <div className="space-y-6"></div>
+    </div>
+  );
+};
+
+export default SearchResalt;
