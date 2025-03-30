ef739dffd77b227084fa44423cc5803b0698e903
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-31 00:09:04 +0530

    update

--- BusTicket_website/src/App.jsx
+++ BusTicket_website/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/bus-tickets" element={<Ticket />} />
+            <Route path="bus-tickets/detail" element={<Detail />}></Route>
           </Routes>
           <Footer />
         </main>
