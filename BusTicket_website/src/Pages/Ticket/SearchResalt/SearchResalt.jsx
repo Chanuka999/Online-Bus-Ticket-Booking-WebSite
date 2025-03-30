f1d493738a14598ff6f384319890f25a221070d9
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-30 13:33:16 +0530

    enter the ticketCard page

--- BusTicket_website/src/Pages/Ticket/SearchResalt/SearchResalt.jsx
+++ BusTicket_website/src/Pages/Ticket/SearchResalt/SearchResalt.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import TicketCard from "../../../Components/Ticket/TicketCard";
+import { FaBus } from "react-icons/fa";
 
 const SearchResalt = () => {
   return (
     <div className="w-full col-span-3 space-y-10 pt-11">
-      <div className="space-y-6"></div>
+      <div className="space-y-6">
+        <TicketCard
+          icon={FaBus}
+          busName={"Sworgadwari Deluxe"}
+          routeFrome={"Kathmandu"}
+          routeTo={"laxapana"}
+          arrivalTime={"6.15 pm"}
+          departureTime={"10.00"}
+          price={"1000"}
+          availableSeats={"5"}
+        />
+      </div>
     </div>
   );
 };
