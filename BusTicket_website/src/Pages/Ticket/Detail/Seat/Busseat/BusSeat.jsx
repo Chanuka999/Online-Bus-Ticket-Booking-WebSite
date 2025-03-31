561922648298ab6be4b14f12bbc62e2ad17ec523
Chanuka999 <chanukaranditha99@gmail.com>
2025-03-31 23:41:14 +0530

    update

--- BusTicket_website/src/Pages/Ticket/Detail/Seat/Busseat/BusSeat.jsx
+++ BusTicket_website/src/Pages/Ticket/Detail/Seat/Busseat/BusSeat.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { GiSteeringWheel } from "react-icons/gi";
 import busSeatData from "../../../../../Constants/Busseat/BusSeatData";
 import { MdOutlineChair } from "react-icons/md";
+import { RiMoneyRupeeCircleLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 const BusSeat = () => {
   const [selectedSeats, setSelectedSeats] = useState([]);
@@ -77,17 +79,136 @@ const BusSeat = () => {
                       </div>
                     ))}
                   </div>
-                  <div className="w-full h-auto grid grid-cols-9 gap-x-5 justify-end"></div>
-                  <div className="w-full h-auto grid grid-cols-9 gap-x-5 justify-end"></div>
-                  <div className="w-full h-auto grid grid-cols-9 gap-x-5 justify-end"></div>
-                  <div className="w-full h-auto grid grid-cols-9 gap-x-5 justify-end"></div>
+                  <div className="w-full h-auto grid grid-cols-9 gap-x-5 justify-end">
+                    {busSeatData.slice(9, 18).map((seat) => (
+                      <div
+                        key={seat.id}
+                        className="flex items-center gap-x-0"
+                        onClick={() => handleSeatClick(seat.id)}
+                      >
+                        <h6 className="text-base text-neutral-600 font-bold">
+                          {seat.id}
+                        </h6>
+                        <MdOutlineChair
+                          className={`text-3xl -rotate-90 ${getSeatName(seat)}`}
+                        />
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="w-full h-auto grid grid-cols-10 gap-x-5 justify-end">
+                    <div className="col-span-9"></div>
+                    {busSeatData.slice(18, 19).map((seat) => (
+                      <div
+                        key={seat.id}
+                        className="flex items-center gap-x-0"
+                        onClick={() => handleSeatClick(seat.id)}
+                      >
+                        <h6 className="text-base text-neutral-600 font-bold">
+                          {seat.id}
+                        </h6>
+                        <MdOutlineChair
+                          className={`text-3xl -rotate-90 ${getSeatName(seat)}`}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  <div className="w-full h-auto grid grid-cols-9 gap-x-5 justify-end">
+                    {busSeatData.slice(19, 28).map((seat) => (
+                      <div
+                        key={seat.id}
+                        className="flex items-center gap-x-0"
+                        onClick={() => handleSeatClick(seat.id)}
+                      >
+                        <h6 className="text-base text-neutral-600 font-bold">
+                          {seat.id}
+                        </h6>
+                        <MdOutlineChair
+                          className={`text-3xl -rotate-90 ${getSeatName(seat)}`}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  <div className="w-full h-auto grid grid-cols-9 gap-x-5 justify-end">
+                    {busSeatData.slice(28, 37).map((seat) => (
+                      <div
+                        key={seat.id}
+                        className="flex items-center gap-x-0"
+                        onClick={() => handleSeatClick(seat.id)}
+                      >
+                        <h6 className="text-base text-neutral-600 font-bold">
+                          {seat.id}
+                        </h6>
+                        <MdOutlineChair
+                          className={`text-3xl -rotate-90 ${getSeatName(seat)}`}
+                        />
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
 
-            <div className="w-full flex items-center gap-6 border-t border-neutral-200 pt-5"></div>
+            <div className="w-full flex items-center gap-6 border-t border-neutral-200 pt-5">
+              <div className="flex items-center gap-x-2">
+                <MdOutlineChair className="text-xl text-primary -rotate-90" />
+                <p className="text-sm text-neutral-500 font-medium">
+                  Available
+                </p>
+              </div>
+
+              <div className="flex items-center gap-x-2">
+                <MdOutlineChair className="text-xl text-primary -rotate-90" />
+                <p className="text-sm text-neutral-500 font-medium">Booked</p>
+              </div>
+
+              <div className="flex items-center gap-x-2">
+                <RiMoneyRupeeCircleLine className="text-xl text-neutral-500 -rotate-90" />
+                <p className="text-sm text-neutral-500 font-medium">
+                  NPR. 1600
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="col-span-2 w-full space-y-5 bg-neutral-50 rounded-xl py-4 px-6 border border-neutral-200 shadow-sm"></div>
+          <div className="col-span-2 w-full space-y-5 bg-neutral-50 rounded-xl py-4 px-6 border border-neutral-200 shadow-sm">
+            <div className="w-full space-y-2">
+              <div className="w-full flex items-center justify-between">
+                <h1 className="text-lg text-neutral-600 font-medium">
+                  Your Destination
+                </h1>
+                <Link
+                  to={"/bus-tickets"}
+                  className="text-sm text-primary font-normal"
+                >
+                  change route
+                </Link>
+              </div>
+
+              <div className="space-y-0 5 w-full">
+                <div className="w-full flex items-center justify-between gap-x-5">
+                  <p className="text-sm text-neutral-400 font-normal">
+                    From <span className="text-x5">(New Buspark)</span>
+                  </p>
+                  <p className="text-sm text-neutral-400 font-normal">
+                    To <span className="text-x5">(ChakChake)</span>
+                  </p>
+                </div>
+
+                <div className="w-full flex items-center justify-between gap-x-4">
+                  <h1 className="text-sm text-neutral-600 font-normal">
+                    Kandy <span className="text-sm font-medium">(6.15 pm)</span>
+                    <div className="flex-1 border-dashbord"></div>
+                  </h1>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full space-y-2"></div>
+
+            <div className="w-full space-y-2"></div>
+
+            <div className="w-full flex items-center justify-center"></div>
+          </div>
         </div>
       </div>
     </div>
