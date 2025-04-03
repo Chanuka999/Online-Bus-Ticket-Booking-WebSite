a095d4c52bfbac7e844ea3510c460ef8bf546121
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-04 01:01:39 +0530

    update

--- BusTicket_website/src/Pages/Ticket/Detail/Detail.jsx
+++ BusTicket_website/src/Pages/Ticket/Detail/Detail.jsx
@@ -4,6 +4,8 @@ import WarningAlert from "../../../Components/AlertMessage.jsx/WarningAlert";
 import TopLayout from "../../../Components/Layout/Toppage/TopLayout";
 import RootLayout from "../../../Components/Layout/RootLayout";
 import BusSeat from "./Seat/Busseat/BusSeat";
+import ToggleBtn from "../../../Components/ToggleBtn/ToggleBtn";
+import Amentities from "./Amentities/Amentities";
 
 const Detail = () => {
   const message = (
@@ -30,7 +32,31 @@ const Detail = () => {
 
           <BusSeat />
         </div>
-        <div className="w-full flex items-center justify-center flex-col gap-8 text-center"></div>
+        <div className="w-full flex items-center justify-center flex-col gap-8 text-center">
+          <p className="text-base text-neutral-500 font-normal text-justify">
+            This is a sample text for the demo purpose. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Sit eaque facere voluptas ipsum
+            beatae id recusandae. Cupiditate, consequuntur ad! Repellendus
+            quaerat nam accusamus repellat obcaecati quis quo placeat soluta
+            unde.
+            <span className="text-lg text-neutral-600 font-medium ml-2">
+              want to see more about
+            </span>
+          </p>
+
+          <div className="w-full flex items-center justify-center gap-6 flex-col">
+            <ToggleBtn
+              buttonText={"see Bus tickets"}
+              buttonTextHidden={"Hide Bus Details"}
+            >
+              <div className="w-full space-y-10">
+                <div className="w-full grid grid-cols-7">
+                  <Amentities />
+                </div>
+              </div>
+            </ToggleBtn>
+          </div>
+        </div>
       </RootLayout>
     </div>
   );
