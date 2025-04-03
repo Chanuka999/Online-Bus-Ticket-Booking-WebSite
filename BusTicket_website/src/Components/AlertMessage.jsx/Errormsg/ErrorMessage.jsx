a253cc278a4cbaaaf4af144017c6f906c3d0dc59
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-03 16:50:40 +0530

    update

--- BusTicket_website/src/Components/AlertMessage.jsx/Errormsg/ErrorMessage.jsx
+++ BusTicket_website/src/Components/AlertMessage.jsx/Errormsg/ErrorMessage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const ErrorMessage = () => {
+const ErrorMessage = ({ message }) => {
   const [isVisible, setIsVisible] = useState(true);
   const [countdown, setCountdown] = useState(10);
 
@@ -20,7 +20,10 @@ const ErrorMessage = () => {
   }
   return (
     <div className="fixed top-28 p-4 right-4 text-sm text-neutral-50 bg-primary rounded-xl shadow-lg transition-transform transform-gpu animate-slide-in">
-      <div className="flex items-center justify-between"></div>
+      <div className="flex items-center justify-between bg-red-300">
+        <span>{message}</span>
+        <span className="ml-4 font-bold">{countdown}</span>
+      </div>
     </div>
   );
 };
