653fc1e26be3df371c545b5b0bdcc3f638f16c85
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-03 16:42:56 +0530

    update

--- BusTicket_website/src/Pages/Ticket/Detail/Seat/Busseat/BusSeat.jsx
+++ BusTicket_website/src/Pages/Ticket/Detail/Seat/Busseat/BusSeat.jsx
@@ -170,44 +170,130 @@ const BusSeat = () => {
               </div>
             </div>
           </div>
-          <div className="col-span-2 w-full space-y-5 bg-neutral-50 rounded-xl py-4 px-6 border border-neutral-200 shadow-sm">
-            <div className="w-full space-y-2">
-              <div className="w-full flex items-center justify-between">
-                <h1 className="text-lg text-neutral-600 font-medium">
-                  Your Destination
-                </h1>
-                <Link
-                  to={"/bus-tickets"}
-                  className="text-sm text-primary font-normal"
-                >
-                  change route
-                </Link>
+        </div>
+        <div className="col-span-2 w-full space-y-5 bg-neutral-50 rounded-xl py-4 px-6 border border-neutral-200 shadow-sm">
+          <div className="w-full space-y-2">
+            <div className="w-full flex items-center justify-between">
+              <h1 className="text-lg text-neutral-600 font-medium">
+                Your Destination
+              </h1>
+              <Link
+                to={"/bus-tickets"}
+                className="text-sm text-primary font-normal"
+              >
+                change route
+              </Link>
+            </div>
+
+            <div className="space-y-0 5 w-full">
+              <div className="w-full flex items-center justify-between gap-x-5">
+                <p className="text-sm text-neutral-400 font-normal">
+                  From <span className="text-x5">(New Buspark)</span>
+                </p>
+                <p className="text-sm text-neutral-400 font-normal">
+                  To <span className="text-x5">(ChakChake)</span>
+                </p>
               </div>
 
-              <div className="space-y-0 5 w-full">
-                <div className="w-full flex items-center justify-between gap-x-5">
-                  <p className="text-sm text-neutral-400 font-normal">
-                    From <span className="text-x5">(New Buspark)</span>
-                  </p>
-                  <p className="text-sm text-neutral-400 font-normal">
-                    To <span className="text-x5">(ChakChake)</span>
-                  </p>
-                </div>
+              <div className="w-full flex items-center justify-between gap-x-4">
+                <h1 className="text-sm text-neutral-600 font-normal">
+                  Kandy <span className="text-sm font-medium">(6.15 pm)</span>
+                </h1>
+                <div className="flex-1 border-dashbord border border-neutral-300" />
+                <h1 className="text-sm text-neutral-600 font-normal">
+                  Laxapana
+                  <span className="text-sm font-medium">(9.15 pm)</span>
+                </h1>
+              </div>
+            </div>
+          </div>
 
-                <div className="w-full flex items-center justify-between gap-x-4">
-                  <h1 className="text-sm text-neutral-600 font-normal">
-                    Kandy <span className="text-sm font-medium">(6.15 pm)</span>
-                    <div className="flex-1 border-dashbord"></div>
-                  </h1>
-                </div>
+          <div className="w-full space-y-2">
+            <div className="w-full flex items-center justify-between">
+              <h1 className="text-lg text-neutral-600 font-medium">
+                Selected Seats
+              </h1>
+
+              <div className="bg-primary/20 rounded-lg py-0.5 px-1.5 text-xs text-xs text-neutral-600 font-normal uppercase">
+                Non-refundable
               </div>
             </div>
 
-            <div className="w-full space-y-2"></div>
+            {selectedSeats.length > 0 ? (
+              <div className="w-full flex items-center gap-x-3">
+                {selectedSeats.map((seatId) => {
+                  return (
+                    <div
+                      key={seatId}
+                      className="w-9 h-9 bg-neutral-200/80 rounded-lg flex items-center justify-center text-base text-neutral-700 font-semibold"
+                    >
+                      {seatId}
+                    </div>
+                  );
+                })}
+              </div>
+            ) : (
+              <div className="w-full flex items-center gap-x-3">
+                <p className="text-sm text-neutral-500 font-normal">
+                  No seat selected
+                </p>
+              </div>
+            )}
+          </div>
 
-            <div className="w-full space-y-2"></div>
+          <div className="w-full space-y-2">
+            <h1 className="text-lg text-neutral-600 font-medium">
+              Fare Details
+            </h1>
+            <div className="w-full flex items-center ustify-between border-dashed border-1-[1.5px] border-neutral-400 pl-2">
+              <h3 className="text-sm text-neutral-500 font-medium">
+                Basic Fair:
+              </h3>
+              <p className="text-sm text-neutral-600 font-medium">LKR.1600</p>
+            </div>
+            <div className="flex items-center justify-between gap-x-4">
+              <div className="flex gap-y-0 5 flex-col">
+                <h3 className="text-base text-neutral-500 font-medium">
+                  Total Price
+                </h3>
+                <span className="text-xs text-neutral-500 font-normal">
+                  (including all taxes)
+                </span>
+              </div>
 
-            <div className="w-full flex items-center justify-center"></div>
+              <p className="text-base text-neutral-600 font-semibold">
+                NPR{""}
+                {selectedSeats.reduce((total, seatId) => {
+                  const seat = busSeatData.find(
+                    (BusSeat) => BusSeat.id === seatId
+                  );
+                  return total + (seat ? seat.price : 0);
+                }, 0)}
+              </p>
+            </div>
+          </div>
+
+          <div className="w-full flex items-center justify-center">
+            {selectedSeats.length > 0 ? (
+              <Link
+                to="/bus-tickets/checkout"
+                className="w-full bg-red-500 hover:bg-primary/90 text-sm text-neutral-50 font-normal py-2.5 flex items-center justify-center uppercase rounded-lg transition"
+              >
+                Processed to Checkout
+              </Link>
+            ) : (
+              <div className="w-full space-y-0.5">
+                <button
+                  disabled
+                  className="w-full bg-red-500 hover:bg-primary/90 text-sm text-neutral-50 font-normal py-2.5 flex items-center justify-center uppercase rounded-lg transition cursor-not-allowed"
+                >
+                  Processed to Checkout
+                </button>
+                <small className="text-xs text-neutral-600 font-normal px-1">
+                  Please select at least one seat to proceed to checkout page
+                </small>
+              </div>
+            )}
           </div>
         </div>
       </div>
