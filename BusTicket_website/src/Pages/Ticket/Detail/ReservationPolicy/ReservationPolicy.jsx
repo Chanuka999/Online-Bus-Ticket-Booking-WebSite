3f0751489f4247c05b67d271c975adbdc0a5b605
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-04 01:08:51 +0530

    update

--- BusTicket_website/src/Pages/Ticket/Detail/ReservationPolicy/ReservationPolicy.jsx
+++ BusTicket_website/src/Pages/Ticket/Detail/ReservationPolicy/ReservationPolicy.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const ReservationPolicy = () => {
-  return <div></div>;
+  return (
+    <div className="col-span-4 w-full border-1 border-neutral-300 pl-5">
+      <div className="w-full space-y-3 text-left">
+        <h1 className="text-lg text-neutral-600 font-medium text-strat">
+          Reservation Policies
+        </h1>
+
+        <ul className="w-full list-disc list-outside space-y-2.5 px-4">
+          <li className="text-sm text-neutral-500 font-normal">
+            Please note that this ticket is non-refundable
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ReservationPolicy;
