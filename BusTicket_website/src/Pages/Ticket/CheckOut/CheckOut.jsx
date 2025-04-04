fac2a68224d305c56be8a7c7a3f317caed920506
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-04 23:07:52 +0530

    update

---
+++ BusTicket_website/src/Pages/Ticket/CheckOut/CheckOut.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PassengerData from "./PassengerData/PassengerData";
+
+const CheckOut = () => {
+  return (
+    <div className="w-full space-y-12 pb-16">
+      <TopLayout
+        bgImg={
+          "https://cdn.pixabay.com/photo/2020/09/21/11/41/bus-5589826_1280.jpg"
+        }
+        title={"checkout"}
+      />
+
+      <RootLayout className="space y-12 w-full pb-16">
+        <div className="w-full grid grid-cols-7 items-start gap-44 relative">
+          <PassengerData />
+        </div>
+      </RootLayout>
+    </div>
+  );
+};
+
+export default CheckOut;
