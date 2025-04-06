05edfa48496dce650e47a4e8cc905168cb6bd5d9
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-06 11:38:51 +0530

    update page

--- BusTicket_website/src/Pages/Ticket/CheckOut/PassengerData/Payment/PaymentMethod.jsx
+++ BusTicket_website/src/Pages/Ticket/CheckOut/PassengerData/Payment/PaymentMethod.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import PaymentCard from "../../../../../Components/Payment/PaymentCard";
+import { FaPlus } from "react-icons/fa";
+
+import MasterCardImg from "../../../../../assets/MasterCard.png";
+import CreditCardImg from "../../../../../assets/creditcard.png";
 
 const PaymentMethod = () => {
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("");
@@ -12,6 +17,32 @@ const PaymentMethod = () => {
       <h6 className="text-sm text-neutral-500 font-mediu">
         Select Payment Method
       </h6>
+
+      <div className="w-full grid grid-cols-2 gap-10">
+        <PaymentCard
+          selectedPayment={selectedPaymentMethod}
+          value={"mastercard"}
+          onChange={handleChange}
+          cardholderName={"Run Bdr.Ghale"}
+          cardNumber={"8888"}
+          cardImage={MasterCardImg}
+        />
+        <PaymentCard
+          selectedPayment={selectedPaymentMethod}
+          value={"creditcard"}
+          onChange={handleChange}
+          cardholderName={"Run Bdr.Ghale"}
+          cardNumber={"8888"}
+          cardImage={CreditCardImg}
+        />
+      </div>
+
+      <div className="w-full flex justify-end">
+        <div className="w-fit flex items-center justify-center gap-x-2 cursor-pointer text-base font-normal text-red-500">
+          <FaPlus />
+          <p className="capitalize">Add new card</p>
+        </div>
+      </div>
     </div>
   );
 };
