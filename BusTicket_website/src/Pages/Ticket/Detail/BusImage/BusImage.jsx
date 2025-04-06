920fab401da63a1a44c9f99694e667de3e5914b1
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-07 01:06:26 +0530

    added

--- BusTicket_website/src/Pages/Ticket/Detail/BusImage/BusImage.jsx
+++ BusTicket_website/src/Pages/Ticket/Detail/BusImage/BusImage.jsx
@@ -5,7 +5,7 @@ const BusImage = () => {
     <div className="w-full grid grid-cols-4 gap-5 items-center border-t border-neutral-300 pt-7 pb-2">
       <div className="w-full rounded-xl overflow-hidden">
         <img
-          src="https://cdn.pixabay.com/photo/2017/07/30/14/44/bus-2554514_1280.jpg"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_L1GngmciVPlN18XJHCMzf7htqaa_aBH21g&s"
           alt="bus images"
           className="w-full aspect-video object-cover object-center rounded-xl border border-neutral-300/50 bg-neutral-200/15"
         />
@@ -13,7 +13,7 @@ const BusImage = () => {
 
       <div className="w-full rounded-xl overflow-hidden">
         <img
-          src="https://cdn.pixabay.com/photo/2017/07/27/19/10/bus-2546383_640.jpg"
+          src="https://lesscarmorelife.com/wp-content/uploads/2023/03/img_6042-1.jpg?w=1024"
           alt="bus images"
           className="w-full aspect-video object-cover object-center rounded-xl border border-neutral-300/50 bg-neutral-200/15"
         />
@@ -21,7 +21,7 @@ const BusImage = () => {
 
       <div className="w-full rounded-xl overflow-hidden">
         <img
-          src="https://cdn.pixabay.com/photo/2017/07/30/14/44/bus-2554514_1280.jpg"
+          src="https://c8.alamy.com/comp/2B488DY/nuwara-eliya-sri-lanka-03212019colorful-bus-for-public-transportruwan-motors-2B488DY.jpg"
           alt="bus images"
           className="w-full aspect-video object-cover object-center rounded-xl border border-neutral-300/50 bg-neutral-200/15"
         />
@@ -29,7 +29,7 @@ const BusImage = () => {
 
       <div className="w-full rounded-xl overflow-hidden">
         <img
-          src="https://cdn.pixabay.com/photo/2018/03/07/16/07/coach-3206326_1280.png"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHOP9-3PN7Pj9dJUpbGcf4V8WwuFX5ywBxAQ&s"
           alt="bus images"
           className="w-full aspect-video object-cover object-center rounded-xl border border-neutral-300/50 bg-neutral-200/15"
         />
